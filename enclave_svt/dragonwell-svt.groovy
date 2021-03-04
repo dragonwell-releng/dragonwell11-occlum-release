0d900f8f900d48c51274c092357d3eb7c11963ea
jeffery.wsj <jeffery.wsj@alibaba-inc.com>
2021-03-04 14:44:31 +0800

    make wget silent

--- enclave_svt/dragonwell-svt.groovy
+++ enclave_svt/dragonwell-svt.groovy
@@ -11,7 +11,7 @@ pipeline {
                      echo 'Download dragonwell resource code and build it'
                      sh "printenv"
                      sh "mkdir -p ${WORKSPACE}/workspace/${BUILD_TAG} && rm -rf ${WORKSPACE}/workspace/${BUILD_TAG}/*"
-                     sh "wget -c ${dragonwell_repo_address} -O - | tar -xz -C ${WORKSPACE}/workspace/${BUILD_TAG} --strip-components 1"
+                     sh "wget -q -c ${dragonwell_repo_address} -O - | tar -xz -C ${WORKSPACE}/workspace/${BUILD_TAG} --strip-components 1"
                      sh "cd ${WORKSPACE}/workspace/${BUILD_TAG} && \
                          mkdir tmp && git clone git@github.com:dragonwell-releng/dragonwell11-occlum-release.git ./tmp && \
                          mv ./tmp/* ./ && rm -rf tmp"
